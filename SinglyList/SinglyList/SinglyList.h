8cf543d8a01fd0372f7e8b53e14151265d6ae9f4
hazemtarekelaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2020-09-12 17:19:58 +0200

    adding some interface functions

--- SinglyList/SinglyList/SinglyList.h
+++ SinglyList/SinglyList/SinglyList.h
@@ -1,5 +1,69 @@
 #pragma once
+#include "Node.h"
+#include <iostream>
 
+template<typename T>
 class SinglyList {
+private:
+	Node* m_head;
+	size_t length;
 
+public:
+	SinglyList() : m_head(nullptr), length(0) {}
+
+	SinglyList(const SinglyList& sList) {
+		
+	}
+
+	SinglyList(SinglyList&& sList) {
+		
+	}
+
+	SinglyList& operator=(const SinglyList& sList) {
+
+	}
+
+	SinglyList& operator=(SinglyList&& sList) {
+
+	}
+
+	size_t GetLength() const { 
+		return length; 
+	}
+
+	friend std::ostream& operator<<(std::ostream& out, const SinglyList& sList) {
+
+	}
+
+	void PushBack(const T& value) {
+
+	}
+
+	void PushFront(const T& value) {
+
+	}
+
+	void PopBack() {
+
+	}
+
+	void PopFront() {
+
+	}
+
+	bool IsEmpty() const {
+
+	}
+
+	void Clear() {
+
+	}
+
+	void Swap(const SinglyList& sList) {
+
+	}
+
+	~SinglyList() {
+
+	}
 };

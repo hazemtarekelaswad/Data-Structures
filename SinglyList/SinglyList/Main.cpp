62a22bc3cd32070b1b10958346faacff09bc9b36
hazemtarekelaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2020-09-13 17:18:04 +0200

    add the rest of constructors and [] operator

--- SinglyList/SinglyList/Main.cpp
+++ SinglyList/SinglyList/Main.cpp
@@ -2,20 +2,11 @@
 #include <forward_list>
 #include "SinglyList.h"
 using namespace std;
+using SL = SinglyList<int>;
 
 int main() {
 	
-	SinglyList<int>* list = new SinglyList<int>;
-	SinglyList<int>* list2 = new SinglyList<int>;
-	list->PushBack(20);
-	list->PushFront(42);
-	list->PushBack(98);
-	list2->PushBack(97);
-	list2->PushBack(91);
-	*list2 = move(*list);
-	cout << *list << endl;
-	cout << *list2 << endl;
-	delete list;
-	delete list2;
+	SL list = { 5, 8, 6, 20 };
+	cout << list[4];
 	return 0;
 }
--- SinglyList/SinglyList/SinglyList.h
+++ SinglyList/SinglyList/SinglyList.h
@@ -13,28 +13,49 @@ public:
 	SinglyList() : m_head(nullptr), length(0) {}
 
 	SinglyList(std::initializer_list<T> initList) {
-		for (auto element = initList.begin(); element != initList.end(); ++element)
-			PushBack(*element);
+		/*typename std::initializer_list<T>::iterator element;
+		for (element = initList.begin(); element != initList.end(); ++element)
+			PushBack(*element);*/
+
+		for (auto element : initList)
+			PushBack(element);
 	}
 
 	SinglyList(const SinglyList& sList) {
-		
+		Node<T>* trav = sList.m_head;
+		while (trav) {
+			this->PushBack(trav->GetValue());
+			trav = trav->GetNextNode();
+		}
 	}
 
 	SinglyList(SinglyList&& sList) {
 		m_head = sList.m_head;
+		length = sList.length;
 		sList.m_head = nullptr;
+		sList.length = 0;
 	}
 
 	SinglyList& operator=(const SinglyList& sList) {
-
+		if (m_head) {
+			~SinglyList();
+			length = 0;
+		}
+		Node<T>* trav = sList.m_head;
+		while (trav) {
+			this->PushBack(trav->GetValue());
+			trav = trav->GetNextNode();
+		}
+		return *this;
 	}
 
 	SinglyList& operator=(SinglyList&& sList) {
 		if(m_head)
-			this->~SinglyList();
+			~SinglyList();
 		m_head = sList.m_head;
+		length = sList.length;
 		sList.m_head = nullptr;
+		sList.length = 0;
 		return *this;
 	}
 
@@ -114,10 +135,25 @@ public:
 		return m_head == nullptr;
 	}
 
-	void Swap(const SinglyList& sList) {
+	T operator[](size_t index) {
+		if (!m_head)
+			throw "invalide index passed\n";
+		Node<T>* trav = m_head;
+		while (index--) {
+			trav = trav->GetNextNode();
+			if(!trav)
+				throw "invalide index passed\n";
+		}
+		return trav->GetValue();
+	}
+
+	void Swap(SinglyList& sList) {
 		Node<T>* tempHead = m_head;
+		size_t tempLength = length;
 		m_head = sList.m_head;
+		length = sList.length;
 		sList.m_head = tempHead;
+		sList.length = tempLength;
 	}
 
 	~SinglyList() {
@@ -125,6 +161,7 @@ public:
 			return;
 		if (!m_head->GetNextNode()) {
 			delete m_head;
+			m_head = nullptr;
 			return;
 		}
 		Node<T>* trav = m_head;
@@ -133,5 +170,6 @@ public:
 			trav = trav->GetNextNode();
 			delete tempHead;
 		}
+		m_head = nullptr;
 	}
 };

a845faaf9bfc1ec774729c065e0729287709d812
hazemtarekelaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2020-09-22 20:49:52 +0200

    add interface of the big 4 and a half

--- DoublyList/DoublyList/DoublyList.h
+++ DoublyList/DoublyList/DoublyList.h
@@ -1,1 +1,42 @@
 #pragma once
+#include "DoublyNode.h"
+#include <iostream>
+#include <initializer_list>
+
+template<typename T>
+class DoublyList {
+private:
+	DoublyNode<T>* m_head;
+	DoublyNode<T>* m_tail;
+	size_t length;
+
+public:
+	DoublyList() {
+
+	}
+
+	DoublyList(const T& value) {
+
+	}
+
+	DoublyList(const std::initializer_list<T>& list) {
+
+	}
+	
+	DoublyList(const DoublyList& list) {
+
+	}
+	
+	DoublyList(DoublyList&& list) {
+
+	}
+
+	DoublyList& operator=(DoublyList& list) {
+
+	}
+
+	~DoublyList() {
+
+	}
+
+};

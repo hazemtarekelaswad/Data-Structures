ee5c6b69a8cec9578e65ddec1efecf6f933d58dd
hazemtarekelaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2020-09-24 21:36:15 +0200

    add pop and push functions and basic constructors

--- DoublyList/DoublyList/DoublyList.h
+++ DoublyList/DoublyList/DoublyList.h
@@ -14,11 +14,12 @@ public:
 	DoublyList() : m_head(nullptr), m_tail(nullptr), length(0) {}
 
 	DoublyList(const T& value) {
-
+		PushBack(value);
 	}
 
 	DoublyList(const std::initializer_list<T>& list) {
-
+		for (auto value : list)
+			PushBack(value);
 	}
 	
 	DoublyList(const DoublyList& list) {
@@ -52,15 +53,51 @@ public:
 	}
 
 	void PushFront(const T& value) {
+		DoublyNode<T>* newNode = new DoublyNode<T>(value);
+		newNode->SetNextNode(m_head);
 
+		if (m_head)
+			m_head->SetPrevNode(newNode);
+
+		m_head = newNode;
+
+		if (!m_tail)
+			m_tail = newNode;
+		++length;
 	}
 
 	void PopBack() {
+		if (!m_tail)
+			return;
 
+		if (!m_tail->GetPrevNode()) {
+			delete m_tail;
+			m_tail = m_head = nullptr;
+		}
+
+		else {
+			m_tail = m_tail->GetPrevNode();
+			delete m_tail->GetNextNode();
+			m_tail->SetNextNode(nullptr);
+		}
+		--length;
 	}
 
 	void PopFront() {
+		if (!m_head)
+			return;
 
+		if (!m_head->GetNextNode()) {
+			delete m_head;
+			m_tail = m_head = nullptr;
+		}
+
+		else {
+			m_head = m_head->GetNextNode();
+			delete m_head->GetPrevNode();
+			m_head->SetPrevNode(nullptr);
+		}
+		--length;
 	}
 
 	T operator[](int index) {
